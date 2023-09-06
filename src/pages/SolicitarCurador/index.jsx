7856c2e20e152a951002cac748f2b427134ea523
Vinicius <maia.viniciusho@gmail.com>
2023-09-05 21:12:22 -0300

    tela solicitar curador

--- src/pages/SolicitarCurador/index.jsx
+++ src/pages/SolicitarCurador/index.jsx
@@ -6,35 +6,17 @@ import styled from 'styled-components';
 import "./style.css";
 
 const SolicitarCurador = () => {
-  window.scrollTo(0, 0); //Reinicia o scroll
+  window.scrollTo(0, 0); // Reinicia o scroll
 
-  // const [mostrarAlerta, setMostrarAlerta] = useState(false);
+  const lidarComAlertaEnviar = () => {
+    alert("Formulário enviado!");
+  };
 
-    const handleAlertaEnviar = () => {
-
-        alert("Form enviado!")
-        // setMostrarAlerta(true);
-        // setTimeout(() => {
-        //     setMostrarAlerta(false);
-        // }, 4000); 
-    };
-  
-  // const CustomButton = styled.button`
-  //   color: #FFF;
-  //   background-color: #DE6B6B;
-  //   font-weight: bold;
-  //   padding: 5px;
-  //   border-radius: 15px;
-  //   font-size: 20px;
-  //   outline: none;
-  //   border: none;
-  // `;
-
-  const FileInput = styled.input`
+  const InputArquivo = styled.input`
     display: none;
   `;
 
-  const SubjectOption = styled.option`
+  const OpcaoDisciplina = styled.option`
     background-color: #343434;
     width: 60px;
     height: 40px;
@@ -46,65 +28,137 @@ const SolicitarCurador = () => {
     float: left;
 
     ${(props) =>
-      props.course === 'CC' &&
-      props.period === '1º Período' &&
+      props.curso === 'CC' &&
+      props.periodo === '1º Período' &&
       `
         background-color: #FF7474;
         color: white;
       `}
 
     ${(props) =>
-      props.course === 'CC' &&
-      props.period === '2º Período' &&
+      props.curso === 'CC' &&
+      props.periodo === '2º Período' &&
       `
         background-color: #8979FD;
         color: white;
       `}
 
     ${(props) =>
-      props.course === 'CC' &&
-      props.selected &&
+      props.curso === 'CC' &&
+      props.periodo === '3º Período' &&
+      `
+        background-color: #F27FEF;
+        color: white;
+      `}
+
+    ${(props) =>
+      props.curso === 'CC' &&
+      props.periodo === '4º Período' &&
+      `
+        background-color: #87E4FE;
+        color: white;
+      `}
+
+    ${(props) =>
+      props.curso === 'CC' &&
+      props.periodo === '5º Período' &&
+      `
+        background-color: #FCD878;
+        color: white;
+      `}
+
+    ${(props) =>
+      props.curso === 'CC' &&
+      props.periodo === '6º Período' &&
+      `
+        background-color: #90FCDF;
+        color: white;
+      `}
+
+    ${(props) =>
+      props.curso === 'CC' &&
+      props.periodo === '7º Período' &&
+      `
+        background-color: #E9FD7A;
+        color: white;
+      `}
+
+    ${(props) =>
+      props.curso === 'CC' &&
+      props.periodo === 'Computação Visual' &&
+      `
+        background-color: #7394FD;
+        color: white;
+      `}
+
+    ${(props) =>
+      props.curso === 'CC' &&
+      props.periodo === 'Sistemas Inteligentes' &&
+      `
+        background-color: #F8B076;
+        color: white;
+      `}
+
+    ${(props) =>
+      props.curso === 'CC' &&
+      props.periodo === 'Sistemas de Computação' &&
+      `
+        background-color: #F27ED6;
+        color: white;
+      `}
+
+    ${(props) =>
+      props.curso === 'CC' &&
+      props.periodo === 'Sistemas de Informação' &&
+      `
+        background-color: #F24430;
+        color: white;
+      `}
+
+    ${(props) =>
+      props.curso === 'CC' &&
+      props.selecionado &&
       `
         background-color: #FFF;
         color: black;
       `}
   `;
 
-  const [selectedFile, setSelectedFile] = useState(null);
-  const [selectedCourse, setSelectedCourse] = useState("Curso");
-  const [selectedPeriod, setSelectedPeriod] = useState("Selecione o período");
-  const [selectedSubjects, setSelectedSubjects] = useState([]);
-  const fileInputRef = useRef(null);
+  const [arquivoSelecionado, setArquivoSelecionado] = useState(null);
+  const [cursoSelecionado, setCursoSelecionado] = useState("Curso");
+  const [periodoSelecionado, setPeriodoSelecionado] = useState("Período");
+  const [disciplinasSelecionadas, setDisciplinasSelecionadas] = useState([]);
+  const referenciaInputArquivo = useRef(null);
 
-  const handleFileChange = (e) => {
-    const file = e.target.files[0];
-    console.log("Arquivo selecionado:", file);
-    setSelectedFile(file);
+  const lidarComMudancaDeArquivo = (e) => {
+    const arquivo = e.target.files[0];
+    console.log("Arquivo selecionado:", arquivo);
+    setArquivoSelecionado(arquivo);
   };
 
-  const handleButtonClick = () => {
-    fileInputRef.current.click();
+  const lidarComCliqueNoBotao = () => {
+    referenciaInputArquivo.current.click();
   };
 
-  const handleCourseChange = (e) => {
-    const selectedCourse = e.target.value;
-    setSelectedCourse(selectedCourse);
-    setSelectedPeriod("Selecione o período");
+  const lidarComMudancaDeCurso = (e) => {
+    const cursoSelecionado = e.target.value;
+    setCursoSelecionado(cursoSelecionado);
+    setPeriodoSelecionado("Período");
   };
 
-  const handlePeriodChange = (e) => {
-    setSelectedPeriod(e.target.value);
-    setSelectedSubjects([]);
+  const lidarComMudancaDePeriodo = (e) => {
+    setPeriodoSelecionado(e.target.value);
+    setDisciplinasSelecionadas([]);
   };
 
-  const coursesData = {
+  const dadosCursos = {
     "Curso": {
-      periods: ["Selecione o período"],
-      subjects: [],
+      periodos: ["Período"],
+      disciplinas: [],
     },
     "CC": {
-      periods: ["Selecione o período", "1º Período", "2º Período", "3º Período", "4º Período", "5º Período", "6º Período", "7º Período", "Computação Visual", "Sistemas Inteligentes", "Sistemas de Computação", "Sistemas de Informação"],
-      subjects: {
+      periodos: ["Período", "1º Período", "2º Período", "3º Período", "4º Período", "5º Período", "6º Período", "7º Período", "Computação Visual", "Sistemas Inteligentes", "Sistemas de Computação", "Sistemas de Informação"],
+      disciplinas: {
         "1º Período": [
           "P1",
           "LÓG",
@@ -117,107 +171,153 @@ const SolicitarCurador = () => {
           "BD",
           "OAC",
           "GA"
+        ],
+        "3º Período": [
+          "REDC",
+          "TG",
+          "PROB",
+          "ÁLG"
+        ],
+        "4º Período": [
+          "P2",
+          "P3",
+          "PAA",
+          "TCOMP"
+        ],
+        "5º Período": [
+          "SO",
+          "COMPI",
+          "IA",
+          "CG"
+        ],
+        "6º Período": ["PDS"],
+        "7º Período": [
+          "MET",
+          "ND"
+        ],
+        "Computação Visual": [
+          "C3",
+          "AM",
+          "REAP",
+          "PDI",
+          "VISC"
+        ],
+        "Sistemas Inteligentes": [
+          "C3",
+          "AMÁQ",
+          "RNAP",
+          "CEVO",
+          "CD"
+        ],
+        "Sistemas de Computação": [
+          "C3",
+          "SDG",
+          "FPGA",
+          "SE",
+          "MICR"
+        ],
+        "Sistemas de Informação": [
+          "CLP",
+          "SDI",
+          "IHM",
+          "Gp",
+          "SEG"
         ]
       },
     },
   };
 
-  const handleSubjectClick = (course, period, subject) => {
-    const subjectIndex = selectedSubjects.findIndex(
-      (item) => item.course === course && item.period === period && item.subject === subject
+  const lidarComCliqueNaDisciplina = (curso, periodo, disciplina) => {
+    const indiceDisciplina = disciplinasSelecionadas.findIndex(
+      (item) => item.curso === curso && item.periodo === periodo && item.disciplina === disciplina
     );
-    if (subjectIndex === -1) {
-      setSelectedSubjects([
-        ...selectedSubjects,
-        { course, period, subject }
+    if (indiceDisciplina === -1) {
+      setDisciplinasSelecionadas([
+        ...disciplinasSelecionadas,
+        { curso, periodo, disciplina }
       ]);
     } else {
-      setSelectedSubjects([
-        ...selectedSubjects.slice(0, subjectIndex),
-        ...selectedSubjects.slice(subjectIndex + 1)
+      setDisciplinasSelecionadas([
+        ...disciplinasSelecionadas.slice(0, indiceDisciplina),
+        ...disciplinasSelecionadas.slice(indiceDisciplina + 1)
       ]);
     }
   };
 
   return (
     <div>
-      <CabecalhoLogado/>
-      <FontePoppins/>
+      <CabecalhoLogado />
+      <FontePoppins />
 
-      <div id="tres">
-        <div id="um">
+      <div id="conteudo-geral">
+        <div id="conteudo-esquerda">
           <div className="titulo-curadoria">Solicitar ser curador</div>
-          <img src={foto} alt="Foto usuário" className='foto-usuario'/>
+          <img src={foto} alt="Foto usuário" className='foto-usuario' />
         </div>
 
-        <div id="dois">
+        <div id="conteudo-direita">
           <form action="">
             <div className="titulo-dados">Olá, <span className="marrom">Vinícius</span>. Envie os dados abaixo:</div>
-            <div className="subtitulo-dados">Em qual matéria você deseja ser curador?</div>
+            <div className="subtitulo-dados">Em qual(is) matéria(s) você deseja ser curador?</div>
 
-            <select value={selectedCourse} onChange={handleCourseChange}>
-              {Object.keys(coursesData).map((course) => (
-                <option key={course} value={course}>
-                  {course}
+            <select value={cursoSelecionado} onChange={lidarComMudancaDeCurso}>
+              {Object.keys(dadosCursos).map((curso) => (
+                <option key={curso} value={curso}>
+                  {curso}
                 </option>
               ))}
             </select>
-            <select value={selectedPeriod} onChange={handlePeriodChange}>
-              {coursesData[selectedCourse].periods.map((period) => (
-                <option key={period} value={period}>
-                  {period}
+            <select value={periodoSelecionado} onChange={lidarComMudancaDePeriodo}>
+              {dadosCursos[cursoSelecionado].periodos.map((periodo) => (
+                <option key={periodo} value={periodo}>
+                  {periodo}
                 </option>
               ))}
             </select>
 
-            {selectedPeriod !== "Selecione o período" && (
+            {periodoSelecionado !== "Período" && (
               <div>
-                <div className="subtitulo-dados">Selecione as disciplinas:</div>
+                <br />
                 <div className="subject-container">
-                  {coursesData[selectedCourse].subjects[selectedPeriod].map((subject) => (
-                    <SubjectOption
-                      key={subject}
-                      value={subject}
-                      course={selectedCourse}
-                      period={selectedPeriod}
-                      selected={selectedSubjects.some((item) => item.course === selectedCourse && item.period === selectedPeriod && item.subject === subject)}
-                      onClick={() => handleSubjectClick(selectedCourse, selectedPeriod, subject)}
+                  {dadosCursos[cursoSelecionado].disciplinas[periodoSelecionado].map((disciplina) => (
+                    <OpcaoDisciplina
+                      key={disciplina}
+                      value={disciplina}
+                      curso={cursoSelecionado}
+                      periodo={periodoSelecionado}
+                      selecionado={disciplinasSelecionadas.some((item) => item.curso === cursoSelecionado && item.periodo === periodoSelecionado && item.disciplina === disciplina)}
+                      onClick={() => lidarComCliqueNaDisciplina(cursoSelecionado, periodoSelecionado, disciplina)}
                     >
-                      {subject}
-                    </SubjectOption>
+                      {disciplina}
+                    </OpcaoDisciplina>
                   ))}
                 </div>
+                <br />
+                <br />
               </div>
             )}
 
             <div>
               <label htmlFor="arquivo" className='subtitulo-dados'>Anexe seu histórico analítico:</label><br />
-              <button type="button" className="custom-button" onClick={handleButtonClick}>
+              <button type="button" className="custom-button" onClick={lidarComCliqueNoBotao}>
                 Selecionar arquivo
               </button>
-              <FileInput
+              <InputArquivo
                 type="file"
                 id="arquivo"
                 name="arquivo"
                 accept=".pdf"
-                onChange={handleFileChange}
-                ref={fileInputRef} 
+                onChange={lidarComMudancaDeArquivo}
+                ref={referenciaInputArquivo}
               />
-              {selectedFile && <div className='arquivo-selecionado'><span className='branco'>Arquivo selecionado:</span> {selectedFile.name}</div>}
+              {arquivoSelecionado && <div className='arquivo-selecionado'><span className='branco'>Arquivo selecionado:</span> {arquivoSelecionado.name}</div>}
             </div>
 
             <div className="subtitulo-dados">Escreva razões de porque você gostaria de ser curador</div>
             <textarea className="text-area" name="comentario" rows="4" cols="25"></textarea><br />
 
-            <button className="enviar" onClick={handleAlertaEnviar}>Enviar</button>
+            <button className="enviar" onClick={lidarComAlertaEnviar}>Enviar</button>
           </form>
-
-          {/* Vai ser mostrado no alerta
-          {mostrarAlerta && (
-              <div className="alertaEnviar">
-              Formulário enviado
-              </div>
-          )} */}
         </div>
       </div>
     </div>
