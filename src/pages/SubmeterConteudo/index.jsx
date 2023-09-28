04ebc51099908671ff35f9951bb6951411861837
vinimholanda <maia.viniciusho@gmail.com>
2023-09-28 00:21:55 -0300

    adicionando todas as disciplinas solicitar curadoria

--- src/pages/SubmeterConteudo/index.jsx
+++ src/pages/SubmeterConteudo/index.jsx
@@ -1,33 +1,29 @@
 import React, { useState, useRef } from 'react';
 import FontePoppins from '../../components/FontePoppins';
 import CabecalhoLogado from "../../components/CabecalhoLogado";
-import styled from 'styled-components';
 import "./style.css";
-
 import BlocoCadaDisciplina from '../../components/BlocoCadaDisciplina';
-import Invariante from '../../components/Pdf/Invariante.pdf';
-
 import Swal from 'sweetalert2';
-
 import { dadosCursos, corPorCursoPeriodo, OpcaoDisciplina, InputArquivo } from './dadosCursos';
 
-const bancoDados = [
-];
+const bancoDados = [];
 
 const SubmeterConteudo = () => {
-  window.scrollTo(0, 0); // Reinicia o scroll
+  // window.scrollTo(0, 0); quebrando
 
   const [arquivoSelecionado, setArquivoSelecionado] = useState(null);
   const [cursoSelecionado, setCursoSelecionado] = useState("Curso");
   const [periodoSelecionado, setPeriodoSelecionado] = useState("Período");
-  const [disciplinasSelecionadas, setDisciplinasSelecionadas] = useState([]);
+  const [disciplinasSelecionadas, setDisciplinasSelecionadas] = useState();
+  const [disciplinaSelecionada, setDisciplinaSelecionada] = useState(null);
+  const [assuntosSelecionados, setAssuntosSelecionados] = useState([]); 
   const referenciaInputArquivo = useRef(null);
 
   const lidarComAlertaEnviar = () => {
     Swal.fire({
         title: 'Conteúdo Enviado!',
         text: 'Quando um curador aprovar ele será publicado.',
-        icon: 'success', // Ícone personalizado (warning, success, error, etc.)
+        icon: 'success',
         confirmButtonText: 'OK',
     });
   };
@@ -51,22 +47,27 @@ const SubmeterConteudo = () => {
   const lidarComMudancaDePeriodo = (e) => {
     setPeriodoSelecionado(e.target.value);
     setDisciplinasSelecionadas([]);
+    setDisciplinaSelecionada(null); 
   };
 
+  const selecionarDisciplina = (curso, periodo, disciplina) => {
+    setDisciplinasSelecionadas([{ curso, periodo, disciplina }]);
+    setDisciplinaSelecionada(disciplina); 
+    setAssuntosSelecionados([]); 
+  };
+  
   const lidarComCliqueNaDisciplina = (curso, periodo, disciplina) => {
-    const indiceDisciplina = disciplinasSelecionadas.findIndex(
-      (item) => item.curso === curso && item.periodo === periodo && item.disciplina === disciplina
+    const disciplinaSelecionada = disciplinasSelecionadas.find(
+      (item) => item.curso === curso && item.periodo === periodo
     );
-    if (indiceDisciplina === -1) {
-      setDisciplinasSelecionadas([
-        ...disciplinasSelecionadas,
-        { curso, periodo, disciplina }
-      ]);
+  
+    if (!disciplinaSelecionada || disciplinaSelecionada.disciplina !== disciplina) {
+      selecionarDisciplina(curso, periodo, disciplina);
     } else {
-      setDisciplinasSelecionadas([
-        ...disciplinasSelecionadas.slice(0, indiceDisciplina),
-        ...disciplinasSelecionadas.slice(indiceDisciplina + 1)
-      ]);
+
+      setDisciplinasSelecionadas([]);
+      setDisciplinaSelecionada(null); 
+      setAssuntosSelecionados([]); 
     }
   };
 
@@ -102,27 +103,32 @@ const SubmeterConteudo = () => {
       <div id="conteudo-geral-submeter">
         <div id="conteudo-esquerda">
           <div className="titulo-submeter">Submeter Conteúdo</div>
-          <p className="paragrofo-explicando"><span className='azul'>Esolha a matéria e o conteúdo, anexe seu PDF, coloque um título </span>
-          e, por fim, envie contéudos para serem aprovados por nosso grupo de curadores.
-            </p>
+          <p className="paragrofo-explicando">
+            <span className='azul'>Escolha a matéria e o conteúdo, anexe seu PDF, coloque um título </span>
+            e, por fim, envie conteúdos para serem aprovados por nosso grupo de curadores.
+          </p>
 
-            <br />
+          <br />
 
-            <p className="paragrofo-explicando">Para serem publicados, esses conteúdos precisam ser aprovados:
-            <span className='marrom'> qualitativamente, eficientemente e eticamente. </span> Além de que, após postados, continuarão a ser avaliados pelos usuários por meio de curtidas e comentários.
-            </p>
+          <p className="paragrofo-explicando">
+            Para serem publicados, esses conteúdos precisam ser aprovados:
+            <span className='marrom'> qualitativamente, eficientemente e eticamente. </span>
+            Além de que, após postados, continuarão a ser avaliados pelos usuários por meio de curtidas e comentários.
+          </p>
 
-            <br />
+          <br />
 
-            <p className="paragrofo-explicando">Lembre-se que você também pode ser curador e ajudar a avaliar os conteúdos, clique em Seja um Curador e veja os pré-requisitos.
-            </p>
+          <p className="paragrofo-explicando">
+            Lembre-se que você também pode ser curador e ajudar a avaliar os conteúdos, clique em Seja um Curador e veja os pré-requisitos.
+          </p>
         </div>
 
-        <span className='test'>&gt;</span>  {/*&gt; == ">"*/}
+        <span className='test'>&gt;</span>
 
         <div id="conteudo-direita-sub">
           <form onSubmit={submitHandler}>
-            <div className="titulo-dados">Olá, <span className="marrom">Vinícius</span>. Preencha abaixo!</div><hr style={{color:"#FFF"}}/>
+            <div className="titulo-dados">Olá, <span className="marrom">Vinícius</span>. Preencha abaixo!</div>
+            <hr style={{color:"#FFF"}}/>
             <div className="subtitulo-dados">1. Em qual matéria você deseja enviar o conteúdo?</div>
 
             <select value={cursoSelecionado} onChange={lidarComMudancaDeCurso}>
@@ -159,11 +165,26 @@ const SubmeterConteudo = () => {
                 </div>
                 <br />
                 <br />
+
+              </div>
+            )}
+
+            {disciplinaSelecionada && (
+              <div>
+                <br />
+
+                <select className="aaa">
+                  {dadosCursos[cursoSelecionado].assuntos[disciplinaSelecionada].map((assunto) => (
+                    <option key={assunto} value={assunto}>
+                      {assunto}
+                    </option>
+                  ))}
+                </select>
               </div>
             )}
 
             <div>
-              <label htmlFor="arquivo" className='subtitulo-dados'>2. Anexe seu conteúdo:</label><br />
+              <label htmlFor="arquivo" className='subtitulo-dados'>3. Anexe seu conteúdo:</label><br />
               <button type="button" className="custom-button" onClick={lidarComCliqueNoBotao}>
                 Selecionar arquivo
               </button>
@@ -178,10 +199,10 @@ const SubmeterConteudo = () => {
               {arquivoSelecionado && <div className='arquivo-selecionado'><span className='branco'>Arquivo selecionado:</span> {arquivoSelecionado.name}</div>}
             </div>
 
-            <div className="subtitulo-dados">3. Título:</div>
+            <div className="subtitulo-dados">4. Título:</div>
             <input type="text" className='titulo-input' value={title} onChange={tituloHandler}/>
 
-            <div className="subtitulo-dados">4. Escreva uma breve descrição:</div>
+            <div className="subtitulo-dados">5. Escreva uma breve descrição:</div>
             <textarea className="text-area" name="comentario" rows="4" cols="25"></textarea><br />
 
             <button type='submit' className="enviar" onClick={lidarComAlertaEnviar}>Enviar</button>
